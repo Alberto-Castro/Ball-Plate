45da75e140c4f4f2550be7a31679cf6488c18536
Alberto-Castro <a22c10g95@gmail.com>
2017-10-24 10:52:02 -0600

    Cambios 24/10/2017

--- PrimerAvance/BallPlate.cydsn/main.c
+++ PrimerAvance/BallPlate.cydsn/main.c
@@ -11,27 +11,46 @@
 */
 #include "project.h"
 #include <stdio.h>
+#include <math.h>
 
-int16 coordx,coordy;
-int16 cuentx,cuenty;
-float datax,datay,ex=0,ey=0,adx=0,ady=0,accx=0,accy=0,accx1=0,accy1=0,accx2=0,accy2=0,ex1=0,ey1=0;
-const float cruce=0, ref=0, Ts=0.0125,escx=0.075,escy=0.091,adc=(5/65536);
+
+int16 coordx,coordy; //Coordenadas medidas de la placa
+int16 cuentx,cuenty; //Salidas al PWM
+int8 touch;
+float datax,datay,ex,ey,adx,ady,accx,accy,accx1,accy1,accx2,accy2,ex1,ey1,adcx,adcy,crucex,crucey; //Variables para el control
+const float ref=0, Ts=0.0125,escx=2,escy=2;
 const float Kpx=0.0813,Kdx=0.0456,Nx=59.1,Kpy=0.06188,Kdy=0.018121,Ny=95.1926;
-char8 str1[12],str2[12],str3[12];
+char8 str1[80],str2[80],str3[80];
 
 void Control(){
     ResistiveTouch_ActivateX();//Configura los pines para medir x
+        
+    //Calculo en Y
+    datay=(adcy*coordy)+crucey;//Conversión (y linealiza) de coordenada a tensión
+    ey=ref-datay;//calcula el error segun la referencia dada
+    ady=-(Ny*Ts-1)*accy1+Kdy*Ny*(ey-ey1);//calculo de la parte derivativa
+    accy=escy*(Kpy*ey+ady);//calculo de la accion de control
+    
+    cuenty=318.31*accy+1500;
+    if (cuenty <= 1000){
+        cuenty = 1000;
+    }
+    if (cuenty >= 2000){
+        cuenty = 2000;
+    }
+   
+    accy1=accy;//guarda la accion de control anterior de y
+    ey1=ey;//guarda el error anterior de y
+    
     coordx=ResistiveTouch_Measure();//toma los datos de la placa
     
     ResistiveTouch_ActivateY();//Configura los pines para medir y
-    coordy=ResistiveTouch_Measure();//toma los datos de la placa
     
     //Calculo en X
-    datax=(adc*coordx)-cruce;//linealiza el valor
+    datax=(adcx*coordx)+(crucex);//Conversión (y linealiza) de coordenada a tensión
     ex=ref-datax;//calcula el error segun la referencia dada
     adx=-(Nx*Ts-1)*accx1+Kdx*Nx*(ex-ex1);//calculo de la parte derivativa
     accx=escx*(Kpx*ex+adx);//calculo de la accion de control
-    //accx2=accx*1.571;
     
     cuentx=318.31*accx+1500;
     if (cuentx <= 1000){
@@ -41,37 +60,31 @@ void Control(){
         cuentx = 2000;
     }
     
-    //Calculo en Y
-    datay=(adc*coordy)-cruce;//toma los datos de la placa y linealiza el valor
-    ey=ref-datay;//calcula el error segun la referencia dada
-    ady=-(Ny*Ts-1)*accy1+Kdy*Ny*(ey-ey1);//calculo de la parte derivativa
-    accy=escy*(Kpy*ey+ady);//calculo de la accion de control
-    //accy2=accy*1.571;
+    accx1=accx;//guarda la accion de control anterior de x
+    ex1=ex;//guarda el error anterior de x
     
-    cuenty=318.31*accy+1500;
-    if (cuenty <= 1000){
-        cuenty = 1000;
-    }
-    if (cuenty >= 2000){
-        cuenty = 2000;
-    }
+    coordy=ResistiveTouch_Measure();//toma los datos de la placa
+        
     
     //Escribe las cuentas en los PWM
     PWM_X_WriteCompare(cuentx);
-    PWM_X_WriteCompare(cuenty);
-    
-    
-    accx1=accx;//guarda la accion de control anterior de x
-    ex1=ex;//guarda el error anterior de x
-    accy1=accy;//guarda la accion de control anterior de y
-    ey1=ey;//guarda el error anterior de y
-    
-    
+    PWM_Y_WriteCompare(cuenty);
 
 }
 
 CY_ISR(isr_Muestreo){
-    Control();
+    touch=ResistiveTouch_TouchDetect();
+    if (touch==1){
+        Control();
+    }
+    else{
+        datax=0;
+        datay=0;
+        cuentx=1500;
+        cuenty=1500;
+        PWM_X_WriteCompare(cuentx);
+        PWM_Y_WriteCompare(cuenty);
+    }
 }
 
 int main(void)
@@ -86,19 +99,19 @@ int main(void)
     CyGlobalIntEnable; /* Enable global interrupts. */
     CY_ISR_PROTO(isr_Muestreo);
     
-   
+    coordx = 0;
+    coordy = 0;
+    datax=0,datay=0,ex=0,ey=0,adx=0,ady=0,accx=0,accy=0,accx1=0,accy1=0,accx2=0,accy2=0,ex1=0,ey1=0,
+    adcx=0.00131,adcy=0.00136,crucex = -2.53, crucey = -2.513;
     
     for(;;)
     {
-        sprintf(str1,"%.7f ",datax);
-        sprintf(str2,"%d",coordx);
-        //sprintf(str3,"%.7f ",accx);
+        sprintf(str1,"Coord.X: %f",datax);
+        sprintf(str2,"Coord.Y: %f",datay);
         LCD_Char_Position(0u,0u);
         LCD_Char_PrintString(str1);
         LCD_Char_Position(1u,0u);
         LCD_Char_PrintString(str2);
-        //LCD_Char_Position(1u,5u);
-        //LCD_Char_PrintString(str3);
         CyDelay(500u);
     }
 }
