740422848a7f5203408b808e24a651c081380fd5
Alberto-Castro <a22c10g95@gmail.com>
2017-10-28 14:47:00 -0600

    28/10/2017

--- PrimerAvance/BallPlate.cydsn/main.c
+++ PrimerAvance/BallPlate.cydsn/main.c
@@ -19,24 +19,28 @@ int16 cuentx,cuenty; //Salidas al PWM
 int8 touch;
 float datax,datay,ex,ey,adx,ady,accx,accy,accx1,accy1,accx2,accy2,ex1,ey1,adcx,adcy,crucex,crucey; //Variables para el control
 const float ref=0, Ts=0.0125,escx=2,escy=2;
-const float Kpx=0.0813,Kdx=0.0456,Nx=59.1,Kpy=0.06188,Kdy=0.018121,Ny=95.1926;
+const float Kpx=0.0803,Kdx=0.0386,Nx=85.1,Kpy=0.0813,Kdy=0.0456,Ny=59.1;//Kpy=0.0813,Kdy=0.0456,Ny=59.1,Kpx=0.0803,Kdx=0.0386,Nx=80.1;
 char8 str1[80],str2[80],str3[80];
 
 void Control(){
+    
     ResistiveTouch_ActivateX();//Configura los pines para medir x
         
     //Calculo en Y
     datay=(adcy*coordy)+crucey;//Conversión (y linealiza) de coordenada a tensión
+    if ((datay<=0.3) & (datay>=-0.3)){
+        datay=0;
+    }
     ey=ref-datay;//calcula el error segun la referencia dada
     ady=-(Ny*Ts-1)*accy1+Kdy*Ny*(ey-ey1);//calculo de la parte derivativa
     accy=escy*(Kpy*ey+ady);//calculo de la accion de control
-
-    cuenty=318.31*accy+1500;
-    if (cuenty <= 1000){
-        cuenty = 1000;
+    
+    cuenty=318.31*accy+1100;
+    if (cuenty <= 991){
+        cuenty = 991;
     }
-    if (cuenty >= 2000){
-        cuenty = 2000;
+    if (cuenty >= 1208){
+        cuenty = 1208;
     }
    
     accy1=accy;//guarda la accion de control anterior de y
@@ -48,23 +52,26 @@ void Control(){
     
     //Calculo en X
     datax=(adcx*coordx)+(crucex);//Conversión (y linealiza) de coordenada a tensión
+    if ((datax<=0.3) & (datax>=-0.3)){
+        datax=0;
+    }
     ex=ref-datax;//calcula el error segun la referencia dada
     adx=-(Nx*Ts-1)*accx1+Kdx*Nx*(ex-ex1);//calculo de la parte derivativa
     accx=escx*(Kpx*ex+adx);//calculo de la accion de control
     
-    cuentx=318.31*accx+1500;
-    if (cuentx <= 1000){
-        cuentx = 1000;
+    
+    cuentx=318.31*accx+1350;
+    if (cuentx <= 1205){
+        cuentx = 1205;
     }
-    if (cuentx >= 2000){
-        cuentx = 2000;
+    if (cuentx >= 1490){
+        cuentx = 1490;
     }
     
     accx1=accx;//guarda la accion de control anterior de x
-    ex1=ex;//guarda el error anterior de x
+    ex1=ex;//guarda el error anterior de x    
     
     coordy=ResistiveTouch_Measure();//toma los datos de la placa
-        
     
     //Escribe las cuentas en los PWM
     PWM_X_WriteCompare(cuentx);
@@ -78,10 +85,12 @@ CY_ISR(isr_Muestreo){
         Control();
     }
     else{
+        accx=0;
+        accy=0;
         datax=0;
         datay=0;
-        cuentx=1500;
-        cuenty=1500;
+        cuentx=1350;
+        cuenty=1100;
         PWM_X_WriteCompare(cuentx);
         PWM_Y_WriteCompare(cuenty);
     }
@@ -102,7 +111,7 @@ int main(void)
     coordx = 0;
     coordy = 0;
     datax=0,datay=0,ex=0,ey=0,adx=0,ady=0,accx=0,accy=0,accx1=0,accy1=0,accx2=0,accy2=0,ex1=0,ey1=0,
-    adcx=0.00131,adcy=0.00136,crucex = -2.53, crucey = -2.513;
+    adcx=0.00131,adcy=0.00136,crucex = -2.53, crucey = -2.91;
     
     for(;;)
     {
